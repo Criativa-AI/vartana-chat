e36a62f0bebd7d796d6d5d009f1b1ab0c974313c
Junior Sereno <juniorsereno.br@gmail.com>
2026-04-07 11:44:20 -0300

    refactor: improve installation_configs trigger logic with explicit casts and modularized configuration values

--- plan_trigger.sql
+++ plan_trigger.sql
@@ -1,23 +1,34 @@
+-- 1. Atualizar valores atuais
 UPDATE installation_configs
-SET serialized_value = to_jsonb('--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: enterprise\n'),
+SET serialized_value = to_jsonb('--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: enterprise
+'::text),
     locked = true
 WHERE name = 'INSTALLATION_PRICING_PLAN';
 
 UPDATE installation_configs
-SET serialized_value = to_jsonb('--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: 10000\n'),
+SET serialized_value = to_jsonb('--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: 10000
+'::text),
     locked = true
 WHERE name = 'INSTALLATION_PRICING_PLAN_QUANTITY';
 
+-- 2. Criar função do trigger com casts explícitos
 CREATE OR REPLACE FUNCTION force_installation_configs_values()
 RETURNS TRIGGER AS $$
+DECLARE
+    plan_val text := '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: enterprise
+';
+    qty_val text := '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: 10000
+';
 BEGIN
     IF NEW.name = 'INSTALLATION_PRICING_PLAN' THEN
-        NEW.serialized_value = to_jsonb('--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: enterprise\n');
+        NEW.serialized_value = to_jsonb(plan_val);
         NEW.locked = true; 
-    END IF;
-
-    IF NEW.name = 'INSTALLATION_PRICING_PLAN_QUANTITY' THEN
-        NEW.serialized_value = to_jsonb('--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: 10000\n');
+    ELSIF NEW.name = 'INSTALLATION_PRICING_PLAN_QUANTITY' THEN
+        NEW.serialized_value = to_jsonb(qty_val);
         NEW.locked = true;
     END IF;
 
@@ -25,6 +36,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- 3. Ativar o trigger
 DROP TRIGGER IF EXISTS trg_keep_installation_configs_fixed ON installation_configs;
 
 CREATE TRIGGER trg_keep_installation_configs_fixed
